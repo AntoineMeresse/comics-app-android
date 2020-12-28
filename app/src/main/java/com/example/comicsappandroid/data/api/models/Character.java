af98a7f8160896955c77e80cfea99699a69c1ce3
Antoine Meresse <ant.meresse@gmail.com>
2020-12-28 02:29:25 +0100

    Character Model

---
+++ app/src/main/java/com/example/comicsappandroid/data/api/models/Character.java
@@ -0,0 +1,10 @@
+package com.example.comicsappandroid.data.api.models;
+
+/**
+ * Character Model
+ * https://comicvine.gamespot.com/api/documentation
+ */
+public class Character {
+    private String name;
+    private Integer id;
+}
