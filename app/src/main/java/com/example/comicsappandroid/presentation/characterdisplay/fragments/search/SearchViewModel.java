f7ee3074364d9e46cf3ca9212d05a63ce7a95142
Antoine Meresse <ant.meresse@gmail.com>
2020-12-28 01:23:58 +0100

    feat: search fragment init

---
+++ app/src/main/java/com/example/comicsappandroid/presentation/characterdisplay/fragments/search/SearchViewModel.java
@@ -0,0 +1,7 @@
+package com.example.comicsappandroid.presentation.characterdisplay.fragments.search;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
