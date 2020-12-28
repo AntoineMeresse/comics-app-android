f43e7433bbee437af0a350f194d4276e2805854e
Antoine Meresse <ant.meresse@gmail.com>
2020-12-28 09:25:32 +0100

    feat: searchCharacters

--- app/src/main/java/com/example/comicsappandroid/data/api/ComicsDisplayService.java
+++ app/src/main/java/com/example/comicsappandroid/data/api/ComicsDisplayService.java
@@ -1,4 +1,12 @@
 package com.example.comicsappandroid.data.api;
 
+import com.example.comicsappandroid.data.api.models.Characters;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ComicsDisplayService {
+    @GET("characters")
+    Single<Characters> searchCharacters(@Query("api_key") String apiKey);
 }
