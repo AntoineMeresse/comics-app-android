81f5f7eb7f63b1bf10b06dcad089da1db0f7d6e3
Antoine Meresse <ant.meresse@gmail.com>
2020-12-28 09:42:40 +0100

    feat: Dependency Injection init Gson

--- app/src/main/java/com/example/comicsappandroid/data/di/FakeDependencyInjection.java
+++ app/src/main/java/com/example/comicsappandroid/data/di/FakeDependencyInjection.java
@@ -2,6 +2,8 @@ package com.example.comicsappandroid.data.di;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
+
 /**
  * This FakeDependencyInjection is inspired from our Android Class Tutorial.
  * This will be used for now and replace if I have time with real dependency injection framework.
@@ -9,6 +11,7 @@ import android.content.Context;
 public class FakeDependencyInjection {
 
     private static Context context;
+    private static Gson jsonSerializer;
 
     /**
      * Method to set a context to the FakeDependencyInjection
@@ -16,6 +19,18 @@ public class FakeDependencyInjection {
      */
     public static void setContext(Context c) {
         context = c;
+    }
+
+    /**
+     * Method to get a JSON serializer. In this app we use GSON
+     * @return Gson
+     */
+    public static Gson getJsonSerializer() {
+        if (jsonSerializer == null) {
+            // Init
+            jsonSerializer = new Gson();
+        }
+        return jsonSerializer;
     }
 
 }
