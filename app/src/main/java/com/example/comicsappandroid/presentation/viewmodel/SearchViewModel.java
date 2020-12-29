c8b7988b787b468ff6037ffb0abac2a2a9fc6d5e
Antoine Meresse <ant.meresse@gmail.com>
2020-12-29 15:59:31 +0100

    feat: MutableLiveData for characters & isDataLoading

--- app/src/main/java/com/example/comicsappandroid/presentation/viewmodel/SearchViewModel.java
+++ app/src/main/java/com/example/comicsappandroid/presentation/viewmodel/SearchViewModel.java
@@ -1,8 +1,12 @@
 package com.example.comicsappandroid.presentation.viewmodel;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.comicsappandroid.data.repository.characterdisplay.CharacterDisplayRepository;
+import com.example.comicsappandroid.presentation.characterdisplay.fragments.search.adapter.CharacterViewItem;
+
+import java.util.List;
 
 public class SearchViewModel extends ViewModel {
 
@@ -10,6 +14,20 @@ public class SearchViewModel extends ViewModel {
 
     public SearchViewModel(CharacterDisplayRepository characterDisplayRepository){
         this.characterDisplayRepository = characterDisplayRepository;
+    }
+
+    // Mutable Live Data
+    private MutableLiveData<List<CharacterViewItem>> characters = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isDataLoading = new MutableLiveData<>();
+
+    // Getters
+
+    public MutableLiveData<Boolean> getIsDataLoading() {
+        return this.isDataLoading;
+    }
+
+    public MutableLiveData<List<CharacterViewItem>> getCharacters(){
+        return this.characters;
     }
 
 }
