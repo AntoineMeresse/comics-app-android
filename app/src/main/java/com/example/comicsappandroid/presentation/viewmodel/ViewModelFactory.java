c904fde38a3239b00d16550a1fc37edf9c29924f
Antoine Meresse <ant.meresse@gmail.com>
2020-12-29 01:27:17 +0100

    feat: View Model Factory init

---
+++ app/src/main/java/com/example/comicsappandroid/presentation/viewmodel/ViewModelFactory.java
@@ -0,0 +1,22 @@
+package com.example.comicsappandroid.presentation.viewmodel;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.comicsappandroid.data.repository.characterdisplay.CharacterDisplayRepository;
+
+public class ViewModelFactory implements ViewModelProvider.Factory {
+
+    private final CharacterDisplayRepository characterDisplayRepository;
+
+    public ViewModelFactory(CharacterDisplayRepository characterDisplayRepository){
+        this.characterDisplayRepository = characterDisplayRepository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return null;
+    }
+}
