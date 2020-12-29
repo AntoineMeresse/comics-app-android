e4911edc983bbc40da633182f6f7f882ec4597b9
Antoine Meresse <ant.meresse@gmail.com>
2020-12-29 13:20:01 +0100

    feat: CharacterViewItem minimum

---
+++ app/src/main/java/com/example/comicsappandroid/presentation/characterdisplay/fragments/search/adapter/CharacterViewItem.java
@@ -0,0 +1,24 @@
+package com.example.comicsappandroid.presentation.characterdisplay.fragments.search.adapter;
+
+public class CharacterViewItem {
+
+    public String getCharacterID() {
+        return characterID;
+    }
+
+    public void setCharacterID(String characterID) {
+        this.characterID = characterID;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    private String characterID;
+    private String characterName;
+
+}
