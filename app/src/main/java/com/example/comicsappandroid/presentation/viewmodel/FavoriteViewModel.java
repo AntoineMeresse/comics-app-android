bb1c595f975aa900d880cd1dd0e52e827172f7dd
Antoine Meresse <ant.meresse@gmail.com>
2020-12-31 00:14:27 +0100

    chore: init FavoriteViewModel

--- app/src/main/java/com/example/comicsappandroid/presentation/viewmodel/FavoriteViewModel.java
+++ app/src/main/java/com/example/comicsappandroid/presentation/viewmodel/FavoriteViewModel.java
@@ -1,7 +1,23 @@
 package com.example.comicsappandroid.presentation.viewmodel;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
+import com.example.comicsappandroid.data.repository.characterdisplay.CharacterDisplayRepository;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableCompletableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class FavoriteViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private CharacterDisplayRepository characterDisplayRepository;
+    private CompositeDisposable compositeDisposable;
+
+    public FavoriteViewModel(CharacterDisplayRepository characterDisplayRepository) {
+        this.characterDisplayRepository = characterDisplayRepository;
+        this.compositeDisposable = new CompositeDisposable();
+    }
 }
