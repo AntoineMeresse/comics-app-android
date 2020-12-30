da5f98f9125fbca8182a0db7357e9092fd141dfb
Antoine Meresse <ant.meresse@gmail.com>
2020-12-30 19:00:40 +0100

    feat: add local methods to CharacterDisplayRepository

--- app/src/main/java/com/example/comicsappandroid/data/repository/characterdisplay/CharacterDisplayRepository.java
+++ app/src/main/java/com/example/comicsappandroid/data/repository/characterdisplay/CharacterDisplayRepository.java
@@ -1,10 +1,24 @@
 package com.example.comicsappandroid.data.repository.characterdisplay;
 
 import com.example.comicsappandroid.data.api.models.CharacterSearchResponse;
+import com.example.comicsappandroid.data.database.CharacterEntity;
 
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
 import io.reactivex.Single;
 
 public interface CharacterDisplayRepository {
     // Remote
     Single<CharacterSearchResponse> getSearchResponse(String filter);
+
+    // Local
+
+    // Completable
+    Completable insertCharacter(String id);
+    Completable deleteCharacter(String id);
+
+    // Flowable
+    Flowable<List<CharacterEntity>> getFavoriteCharacters();
 }
