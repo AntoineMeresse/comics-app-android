8b49fb24f980159a85c0c7d806ea590a94612da0
Antoine Meresse <ant.meresse@gmail.com>
2020-12-31 00:01:43 +0100

    feat: CharacterDisplayDataRepository insertCharacter

--- app/src/main/java/com/example/comicsappandroid/data/repository/characterdisplay/CharacterDisplayDataRepository.java
+++ app/src/main/java/com/example/comicsappandroid/data/repository/characterdisplay/CharacterDisplayDataRepository.java
@@ -3,13 +3,16 @@ package com.example.comicsappandroid.data.repository.characterdisplay;
 import com.example.comicsappandroid.data.api.models.CharacterSearchResponse;
 import com.example.comicsappandroid.data.database.CharacterEntity;
 import com.example.comicsappandroid.data.repository.characterdisplay.local.CharacterDisplayLocalDS;
+import com.example.comicsappandroid.data.repository.characterdisplay.mapper.MapperCharacterToCharacterEntity;
 import com.example.comicsappandroid.data.repository.characterdisplay.remote.CharacterDisplayRemoteDS;
 
 import java.util.List;
 
 import io.reactivex.Completable;
+import io.reactivex.CompletableSource;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
+import io.reactivex.functions.Function;
 
 public class CharacterDisplayDataRepository implements CharacterDisplayRepository {
 
@@ -33,7 +36,28 @@ public class CharacterDisplayDataRepository implements CharacterDisplayRepositor
 
     @Override
     public Completable insertCharacter(String id) {
-        return null;
+        // step 1:
+        Single<CharacterSearchResponse> characterSearchResponseSingle =
+                characterDisplayRemoteDS.searchCharacters("id:"+id);
+
+        // step 2:
+        Single<CharacterEntity> characterEntitySingle = characterSearchResponseSingle.map(
+                new Function<CharacterSearchResponse, CharacterEntity>() {
+            @Override
+            public CharacterEntity apply(CharacterSearchResponse characterSearchResponse) throws Exception {
+                return MapperCharacterToCharacterEntity.convertCharacterToCharacterEntity(characterSearchResponse);
+            }
+        });
+
+        // step 3:
+        Completable characterCompletable = characterEntitySingle.flatMapCompletable(new Function <CharacterEntity, CompletableSource>(){
+            @Override
+            public CompletableSource apply(CharacterEntity characterEntity) throws Exception {
+                return characterDisplayLocalDS.insertCharacter(characterEntity);
+            }
+        });
+
+        return characterCompletable;
     }
 
     @Override
