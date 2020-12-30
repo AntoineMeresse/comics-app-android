bb28875cdc89d83aca19e27775af8889012fbca8
Antoine Meresse <ant.meresse@gmail.com>
2020-12-30 23:33:27 +0100

    feat: fix MapperCharacterToCharacterEntity

--- app/src/main/java/com/example/comicsappandroid/data/repository/characterdisplay/mapper/MapperCharacterToCharacterEntity.java
+++ app/src/main/java/com/example/comicsappandroid/data/repository/characterdisplay/mapper/MapperCharacterToCharacterEntity.java
@@ -1,15 +1,19 @@
 package com.example.comicsappandroid.data.repository.characterdisplay.mapper;
 
 import com.example.comicsappandroid.data.api.models.CharacterComics;
+import com.example.comicsappandroid.data.api.models.CharacterSearchResponse;
 import com.example.comicsappandroid.data.database.CharacterEntity;
 
 public class MapperCharacterToCharacterEntity {
 
-    public static CharacterEntity convertCharacterToCharacterEntity(CharacterComics character) {
+    public static CharacterEntity convertCharacterToCharacterEntity(CharacterSearchResponse characterSearchResponse) {
         CharacterEntity res = new CharacterEntity();
-        res.setId(character.getId());
-        res.setName(character.getName());
-        res.setImageUrl(character.getCharacterImage().getMediumUrl());
+        if(characterSearchResponse.getCharacterList().size() > 0) {
+            CharacterComics character = characterSearchResponse.getCharacterList().get(0);
+            res.setId(character.getId());
+            res.setName(character.getName());
+            res.setImageUrl(character.getCharacterImage().getMediumUrl());
+        }
         return res;
     }
 }
