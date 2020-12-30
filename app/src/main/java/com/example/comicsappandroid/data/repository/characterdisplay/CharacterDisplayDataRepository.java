b63c34feaf391bc26a82fa21b0e0fe44b6449375
Antoine Meresse <ant.meresse@gmail.com>
2020-12-30 19:04:30 +0100

    chore: implement methods for local datasource in CharacterDisplayDataRepository

--- app/src/main/java/com/example/comicsappandroid/data/repository/characterdisplay/CharacterDisplayDataRepository.java
+++ app/src/main/java/com/example/comicsappandroid/data/repository/characterdisplay/CharacterDisplayDataRepository.java
@@ -1,20 +1,48 @@
 package com.example.comicsappandroid.data.repository.characterdisplay;
 
 import com.example.comicsappandroid.data.api.models.CharacterSearchResponse;
+import com.example.comicsappandroid.data.database.CharacterEntity;
+import com.example.comicsappandroid.data.repository.characterdisplay.local.CharacterDisplayLocalDS;
 import com.example.comicsappandroid.data.repository.characterdisplay.remote.CharacterDisplayRemoteDS;
 
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
 import io.reactivex.Single;
 
 public class CharacterDisplayDataRepository implements CharacterDisplayRepository {
 
     private CharacterDisplayRemoteDS characterDisplayRemoteDS;
+    private CharacterDisplayLocalDS characterDisplayLocalDS;
 
-    public CharacterDisplayDataRepository(CharacterDisplayRemoteDS characterDisplayRemoteDS) {
+    public CharacterDisplayDataRepository(CharacterDisplayRemoteDS characterDisplayRemoteDS,
+                                          CharacterDisplayLocalDS characterDisplayLocalDS) {
         this.characterDisplayRemoteDS = characterDisplayRemoteDS;
+        this.characterDisplayLocalDS = characterDisplayLocalDS;
     }
+
+    // ---------------------------------- Remote ------------------------------------------------
 
     @Override
     public Single<CharacterSearchResponse> getSearchResponse(String filter) {
         return characterDisplayRemoteDS.searchCharacters(filter);
+    }
+
+    // ---------------------------------- Local ------------------------------------------------
+
+    @Override
+    public Completable insertCharacter(String id) {
+        return null;
+    }
+
+    @Override
+    public Completable deleteCharacter(String id) {
+        return null;
+    }
+
+    @Override
+    public Flowable<List<CharacterEntity>> getFavoriteCharacters() {
+        return null;
     }
 }
