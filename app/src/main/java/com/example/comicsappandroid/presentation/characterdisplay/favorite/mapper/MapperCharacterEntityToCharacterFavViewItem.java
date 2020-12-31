68c482ab6fa6ebbbafb2be3a8675aeb0167aecda
Antoine Meresse <ant.meresse@gmail.com>
2020-12-31 16:50:34 +0100

    feat: Mapper CharacterEntity to CharacterFavViewItem

---
+++ app/src/main/java/com/example/comicsappandroid/presentation/characterdisplay/favorite/mapper/MapperCharacterEntityToCharacterFavViewItem.java
@@ -0,0 +1,29 @@
+package com.example.comicsappandroid.presentation.characterdisplay.favorite.mapper;
+
+import com.example.comicsappandroid.data.database.CharacterEntity;
+import com.example.comicsappandroid.presentation.characterdisplay.favorite.adapter.CharacterFavViewItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapperCharacterEntityToCharacterFavViewItem {
+
+    private CharacterFavViewItem characterFavViewItem;
+    private List<CharacterFavViewItem> characterFavViewItemList;
+
+    public CharacterFavViewItem map(CharacterEntity character) {
+        characterFavViewItem = new CharacterFavViewItem();
+        characterFavViewItem.setCharacterID(character.getId().toString());
+        characterFavViewItem.setCharacterName(character.getName());
+        characterFavViewItem.setCharacterImageUrl(character.getImageUrl());
+        return characterFavViewItem;
+    }
+
+    public List<CharacterFavViewItem> map(List<CharacterEntity> characterEntities) {
+        characterFavViewItemList = new ArrayList<>();
+        for(CharacterEntity cc : characterEntities) {
+            characterFavViewItemList.add(map(cc));
+        }
+        return characterFavViewItemList;
+    }
+}
