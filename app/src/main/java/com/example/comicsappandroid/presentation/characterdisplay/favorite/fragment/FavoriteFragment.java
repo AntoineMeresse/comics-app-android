c0b4de9cc438cea60b449c626a21cf78f16e71d1
Antoine Meresse <ant.meresse@gmail.com>
2020-12-31 16:29:34 +0100

    feat: Favorite Fragment + layout

--- app/src/main/java/com/example/comicsappandroid/presentation/characterdisplay/favorite/fragment/FavoriteFragment.java
+++ app/src/main/java/com/example/comicsappandroid/presentation/characterdisplay/favorite/fragment/FavoriteFragment.java
@@ -7,17 +7,26 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.comicsappandroid.R;
+import com.example.comicsappandroid.presentation.characterdisplay.favorite.adapter.CharacterFavActionInterface;
+import com.example.comicsappandroid.presentation.characterdisplay.favorite.adapter.CharacterFavAdapter;
 import com.example.comicsappandroid.presentation.viewmodel.FavoriteViewModel;
 
-public class FavoriteFragment extends Fragment {
+public class FavoriteFragment extends Fragment implements CharacterFavActionInterface {
 
-    private FavoriteViewModel mViewModel;
+    private FavoriteViewModel favoriteViewModel;
+    private View rootView;
+
+    private CharacterFavAdapter characterFavAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
     public static FavoriteFragment newInstance() {
         return new FavoriteFragment();
@@ -26,12 +35,27 @@ public class FavoriteFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.favorite_fragment, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        rootView = inflater.inflate(R.layout.favorite_fragment, container, false);
+        return rootView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        setupRecyclerView();
     }
 
+    private void setupRecyclerView() {
+        this.recyclerView = rootView.findViewById(R.id.recycler_view_favorite);
+        this.characterFavAdapter = new CharacterFavAdapter(this, getContext());
+        this.linearLayoutManager = new LinearLayoutManager(getContext());
+        this.recyclerView.setAdapter(characterFavAdapter);
+        this.recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    @Override
+    public void removeFromFavorite(String id) {
+        favoriteViewModel.deleteFromFavorite(id);
+    }
 }
