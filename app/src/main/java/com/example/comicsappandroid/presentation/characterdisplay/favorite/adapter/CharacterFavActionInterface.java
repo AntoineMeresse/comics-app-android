4ed0fac268b16d8b5508b2f355421c64556651ad
Antoine Meresse <ant.meresse@gmail.com>
2020-12-31 13:20:06 +0100

    feat: favorite Action interface

---
+++ app/src/main/java/com/example/comicsappandroid/presentation/characterdisplay/favorite/adapter/CharacterFavActionInterface.java
@@ -0,0 +1,6 @@
+package com.example.comicsappandroid.presentation.characterdisplay.favorite.adapter;
+
+public interface CharacterFavActionInterface {
+    // Method to delete an item from favorite
+    void removeFromFavorite(String id);
+}
