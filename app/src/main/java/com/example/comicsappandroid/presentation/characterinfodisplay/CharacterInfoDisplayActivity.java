4c068ef2446b2d0ad522bfb14e5083c56bfae810
Antoine Meresse <ant.meresse@gmail.com>
2021-01-01 14:45:59 +0100

    feat: init Character Info Display Activity

---
+++ app/src/main/java/com/example/comicsappandroid/presentation/characterinfodisplay/CharacterInfoDisplayActivity.java
@@ -0,0 +1,15 @@
+package com.example.comicsappandroid.presentation.characterinfodisplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.comicsappandroid.R;
+
+public class CharacterInfoDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_info_display);
+    }
+}
